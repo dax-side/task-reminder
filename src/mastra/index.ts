f9f24af33eef3cc9d07f2d7a4721f8772f5d6986
damola <damolaadegbite0@gmail.com>
2025-03-07 00:25:55 +0100

    feat: initialize task reminder project

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,10 +2,10 @@
 import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
 
-import { weatherAgent } from './agents';
-
+import { weatherAgent} from './agents';
+import { taskReminderAgent} from './agents/reminder';
 export const mastra = new Mastra({
-  agents: { weatherAgent },
+  agents: { weatherAgent, taskReminderAgent },
   logger: createLogger({
     name: 'Mastra',
     level: 'info',
